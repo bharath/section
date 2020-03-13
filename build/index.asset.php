029a365b9bcf18ff330e6aaff0a3e51ca141a56e
Bharath <obharath@gmail.com>
2020-03-13 13:33:26 +0530

    linting

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'e816218edf71a3754fd8a3d06e0e600c');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'cbc101b3835fbbedf1d9d8c775c72fcb');
