bb8285798ca45ecd47c60cc6abfb3fcd37afcd13
Bharath <obharath@gmail.com>
2020-03-24 13:25:14 +0530

    Add example pattern

--- section.php
+++ section.php
@@ -66,3 +66,11 @@ function oleti_section_block_init() {
 	) );
 }
 add_action( 'init', 'oleti_section_block_init' );
+
+register_pattern(
+    'oleti/section',
+    array(
+        'title'   => __( 'Three buttons' ),
+        'content' => "<!-- wp:buttons {\"align\":\"center\"} -->\n<div class=\"wp-block-buttons aligncenter\"><!-- wp:button {\"backgroundColor\":\"very-dark-gray\",\"borderRadius\":0} -->\n<div class=\"wp-block-button\"><a class=\"wp-block-button__link has-background has-very-dark-gray-background-color no-border-radius\">Button One</a></div>\n<!-- /wp:button -->\n\n<!-- wp:button {\"textColor\":\"very-dark-gray\",\"borderRadius\":0,\"className\":\"is-style-outline\"} -->\n<div class=\"wp-block-button is-style-outline\"><a class=\"wp-block-button__link has-text-color has-very-dark-gray-color no-border-radius\">Button Two</a></div>\n<!-- /wp:button --></div>\n<!-- /wp:buttons -->",
+    )
+);
